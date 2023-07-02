e07045ad6d654571bc596b02ff61a7995c6ae11d
FedorSherbinin <fredersherb@gmail.com>
2023-07-02 16:47:50 +0300

    Example 006 Condition if else

---
+++ Example006_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "fedor")
+{
+    Console.WriteLine("Ура, это же Fedor!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
